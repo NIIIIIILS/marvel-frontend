564817c2b20a429b880319c4bdc7d8c716b69413
NIIIIIILS <oportonils@gmail.com>
2025-06-15 23:29:01 +0200

    modif home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,7 @@
-import spidy from "../imgs/spidy.png";
-
 const Home = () => {
   return (
     <main>
       <h1>Salut à toi, jeune heros 🦸‍♂️</h1>
-      <img src={spidy} alt="spiderman" />
     </main>
   );
 };
