009a85a31cc342d46f2f100d09cb716c657b0b8a
NIIIIIILS <oportonils@gmail.com>
2025-06-15 23:25:11 +0200

    Second commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,12 @@
+import spidy from "../imgs/spidy.png";
+
 const Home = () => {
-  return <h1>Salut à toi, jeune heros 🦸‍♂️</h1>;
+  return (
+    <main>
+      <h1>Salut à toi, jeune heros 🦸‍♂️</h1>
+      <img src={spidy} alt="spiderman" />
+    </main>
+  );
 };
 
 export default Home;
